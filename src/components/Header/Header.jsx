e8c4c4d0891cf7d86dda116574ff314243f5d9a1
Dmytro Oleksandryuk <53580592+dimaoleks@users.noreply.github.com>
2021-03-31 20:12:15 +0300

    LES-61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
-    return <header className={s.header}>
-        <img src='https://cdn.shopify.com/shopifycloud/hatchful-web/assets/67cbe9b74baf7f893488c5fc426d31eb.png' alt=''></img>
-    </header>
+const Header = (props) => {
+    return (
+        <header className={s.header}>
+            <img src='https://cdn.shopify.com/shopifycloud/hatchful-web/assets/67cbe9b74baf7f893488c5fc426d31eb.png'
+                 alt=''/>
+            <div className={s.loginBlock}>
+                {
+                    props.isAuth
+                        ? props.login
+                        : <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
+        </header>
+    );
 }
 
 export default Header;
