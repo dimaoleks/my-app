70bffc4cea07b6271baca515fa403e1a2c77bdbd
Dmytro Oleksandryuk <53580592+dimaoleks@users.noreply.github.com>
2021-04-01 19:59:43 +0300

    Revert "Update Users.jsx"
    This reverts commit db54dfc27a725e0b4071c477112972cccaa6cb50.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -45,7 +45,6 @@ let Users = (props) => {
                                 : <button onClick={() => {
                                     usersAPI.setFollow(u.id)
                                         .then(response => {
-                                            if (response.resultCode === 0) {
                                                 props.follow(u.id);
                                             }
                                         });
