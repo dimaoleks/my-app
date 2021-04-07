24856c6dd1aeb8b2f03ce5422669a7f324bede22
Dmytro Oleksandryuk <53580592+dimaoleks@users.noreply.github.com>
2021-04-07 20:00:36 +0300

    LES-71

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import s from './ProfileInfo.module.css';
 import Preloader from '../../../common/Preloader/Preloader';
 import React from 'react';
 import userPhoto from "../../../../assets/images/user-logo.png";
-
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -10,14 +10,14 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'
-                    alt=''/>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'*/}
+            {/*        alt=''/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img className={s.userPhoto} src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} alt=''/>
-                description
+                <ProfileStatus status={"hello my friends"}/>
             </div>
         </div>
     );
---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,48 @@
+import s from "./ProfileInfo.module.css";
+import React from "react";
+
+class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    };
+
+    activateEditMode() {
+        //setState is asynchronously
+        this.setState({
+            editMode: true
+        });
+    }
+
+    deactivateEditMode() {
+        //setState is asynchronously
+        this.setState({
+            editMode: false
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    !this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={() => {
+                            this.activateEditMode();
+                        }}>{this.props.status}</span>
+                    </div>
+                }
+                {
+                    this.state.editMode &&
+                    <div>
+                        <input autoFocus={true} onBlur={() => {
+                            this.deactivateEditMode();
+                        }} value={this.props.status}/>
+                    </div>
+                }
+            </div>
+        );
+    }
+
+}
+
+export default ProfileStatus;
