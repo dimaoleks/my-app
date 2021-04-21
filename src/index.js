4a6bf9d4d931a45a771d70f8b7298e65b79a31bb
Dmytro Oleksandryuk <53580592+dimaoleks@users.noreply.github.com>
2021-04-21 22:03:56 +0300

    LES-92

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/redux-store';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from "react-redux";
+import MainApp from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
+        <MainApp/>
     </React.StrictMode>,
     document.getElementById('root')
 );
